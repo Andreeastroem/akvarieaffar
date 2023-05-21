497fe0b2d3938102623b2c7b3a1a82fdfc0348db
kjof <andreeastroem@gmail.com>
2023-05-21 13:33:57 +0200

    fix: add disclaimers on pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
-import Link from "next/link"
-
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center p-24 gap-6">
-      <h2>
-        <Link href={"/fisk"} className="text-blue-400 hover:text-blue-200">
-          👉 Link to start of navigation
-        </Link>
-      </h2>
-      <h2>
-        <Link href={"/om-affaren"} className="text-blue-400 hover:text-blue-200">
-          👉 Om affären
-        </Link>
-      </h2>
+      <h2>Se upp!</h2>
+      <p>Hemsidan är under konstruktion och inte användbar för tillfället.</p>
+      <p>Det kan därför finnas fallgropar 🕳️ och brutna länkar ꝇ</p>
+      <p>Tack för du tittade in 👋</p>
     </main>
   )
 }
