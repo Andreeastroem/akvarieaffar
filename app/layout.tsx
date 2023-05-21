9ca65bf848802f003459981f0a7b44169c62c9f9
kjof <andreeastroem@gmail.com>
2023-05-21 13:43:53 +0200

    fix: add disclaimer

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="sv">
       <ClerkProvider>
         <body className="max-w-7xl mx-auto dark-gradient text-gray-200 p-3 min-h-screen">
+          <div className="bg-red-300/75 -mx-3 -mt-3 px-3 py-2 whitespace-nowrap overflow-x-hidden">
+            <span className="w-full animate-marquee inline-block">
+              Hemsidan är under uppbyggnad 👷‍♂️
+            </span>
+            <span className="w-full animate-marquee inline-block">
+              Hemsidan är under uppbyggnad 👷‍♂️
+            </span>
+          </div>
           {/** @ts-expect-error Server Component */}
           <Header />
           {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        marquee: "marquee 20s linear infinite",
       },
     },
   },
