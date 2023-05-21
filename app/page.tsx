9f0550aa153f5c8d9e6c710ef6fdb026373485ec
kjof <andreeastroem@gmail.com>
2023-05-21 13:52:54 +0200

    fix: change padding

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center p-24 gap-6">
+    <main className="flex h-full flex-col items-center justify-center p-6 gap-6">
       <h2>Se upp!</h2>
       <p>Hemsidan är under konstruktion och inte användbar för tillfället.</p>
       <p>Det kan därför finnas fallgropar 🕳️ och brutna länkar ꝇ</p>
