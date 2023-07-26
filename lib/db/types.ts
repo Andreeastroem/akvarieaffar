40ddad22345f2c822223f1e6019feee900a7d369
kjof <andreeastroem@gmail.com>
2023-07-26 23:19:04 +0200

    fix: change to CMS names in types

--- lib/db/types.ts
+++ lib/db/types.ts
@@ -1,11 +1,14 @@
+import { StructuredTextDocument } from "react-datocms"
+
 export type Continent = "Afrika" | "Sydamerika" | "Nordamerika" | "Europa" | "Asien"
-export type Diet = "allätare" | "växtätare"
-export type WaterType = "sötvatten" | "saltvatten"
+export type Diet = "allätare" | "växtätare" | "köttätare"
+export type WaterType = "sötvatten" | "saltvatten" | "brackvatten"
+export type DepthLevel = "botten" | "mellan" | "yta"
 
 export type Fish = {
   scientificName: string
-  continent: Continent
-  origin: string
+  continentOfOrigin: Continent
+  countryOfOrigin: string
   aquarium_min_size: number
   aquarium_min_volume: number
   min_temperature: number
@@ -19,4 +22,29 @@ export type Fish = {
   commonName: string
   family: string
   id: number
+  description: {
+    value: StructuredTextDocument
+  }
+  mainImage: {
+    alt: string
+    url: string
+  }
+  waterType: string
+  temperature: {
+    min: number
+    max: number
+  }
+  socialNeeds: string
+  ph: {
+    min: number
+    max: number
+  }
+  additionalImages: {
+    alt: string
+    url: string
+  }
+  aquariumMinSize: number
+  aquariumMinVolume: number
+  depth: DepthLevel
+  genderDifferences: string
 }
