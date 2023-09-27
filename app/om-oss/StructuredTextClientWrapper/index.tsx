4812804fa3145467e829a7b689e902892853c6e9
kjof <andreeastroem@gmail.com>
2023-09-27 08:29:17 +0200

    fix: typescript error

--- app/om-oss/StructuredTextClientWrapper/index.tsx
+++ app/om-oss/StructuredTextClientWrapper/index.tsx
@@ -2,6 +2,12 @@
 
 import { StructuredText, StructuredTextDocument } from "react-datocms/structured-text"
 
-export default function StructuredTextClientWrapper({ data }: { data: StructuredTextDocument }) {
+export default function StructuredTextClientWrapper({
+  data,
+}: {
+  data: {
+    value: StructuredTextDocument
+  }
+}) {
   return <StructuredText data={data} />
 }
