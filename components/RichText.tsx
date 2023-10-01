187909f201c2ebddc37e3632c1a05247efb090f2
kjof <andreeastroem@gmail.com>
2023-10-01 17:13:38 +0200

    fix: article page rich text format

--- components/RichText.tsx
+++ components/RichText.tsx
@@ -1,4 +1,4 @@
-import { isList } from "datocms-structured-text-utils"
+import { isHeading, isList, isListItem } from "datocms-structured-text-utils"
 import {
   renderNodeRule,
   StructuredText,
@@ -10,12 +10,37 @@ export function RichText({ content }: { content: StructuredTextDocument }) {
     <StructuredText
       customNodeRules={[
         renderNodeRule(isList, ({ node, children }) => {
-          console.log(node.style)
           switch (node.style) {
             case "numbered":
-              return <ol></ol>
+              return (
+                <ol className="list-decimal grid gap-3 italic max-w-xl mx-auto py-6">{children}</ol>
+              )
             case "bulleted":
-              return <ul className="list-disc ml-5">{children}</ul>
+              return <ul className="list-disc grid gap-3 italic max-w-xl mx-auto">{children}</ul>
+          }
+        }),
+        renderNodeRule(isListItem, ({ node, children, key }) => {
+          console.log("node", node.type)
+          return (
+            <li className="" key={key}>
+              {children}
+            </li>
+          )
+        }),
+        renderNodeRule(isHeading, ({ node, children }) => {
+          switch (node.level) {
+            case 1:
+              return <h1 className="text-3xl font-bold">{children}</h1>
+            case 2:
+              return <h2 className="text-2xl font-bold">{children}</h2>
+            case 3:
+              return <h3 className="text-xl font-bold">{children}</h3>
+            case 4:
+              return <h4 className="text-lg font-bold">{children}</h4>
+            case 5:
+              return <h5 className="text-base font-bold">{children}</h5>
+            case 6:
+              return <h6 className="text-sm font-bold">{children}</h6>
           }
         }),
       ]}
