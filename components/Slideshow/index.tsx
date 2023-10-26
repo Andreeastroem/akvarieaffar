c82c478d1bbbcb25de5b7809d885abc0ab614ea7
kjof <andreeastroem@gmail.com>
2023-10-26 22:20:16 +0200

    fix: hide scrollbar

--- components/Slideshow/index.tsx
+++ components/Slideshow/index.tsx
@@ -8,14 +8,14 @@ import { DatoImage } from "@/lib/cms/types"
 export default function Slideshow({ images }: { images: Array<DatoImage> }) {
   const [shownImageIndex, setShownImageIndex] = useState(0)
   const [thumbnailClicked, setThumbnailClicked] = useState(false)
-  const slideshowRef = useRef<HTMLDivElement>(null)
+  const slideshowRef = useRef<HTMLUListElement>(null)
 
   if (images.length === 0) return null
 
   return (
     <div className="w-fit">
-      <div
-        className="relative snap-x snap-mandatory max-w-[400px] flex overflow-scroll"
+      <ul
+        className="relative scrollbar-hide snap-x snap-mandatory max-w-[400px] flex overflow-scroll"
         onScroll={(e) => {
           if (thumbnailClicked) return
           setShownImageIndex(Math.round(e.currentTarget.scrollLeft / e.currentTarget.clientWidth))
@@ -24,7 +24,7 @@ export default function Slideshow({ images }: { images: Array<DatoImage> }) {
       >
         {images.map((image, idx) => {
           return (
-            <div className="relative snap-start min-w-[400px] w-full aspect-[1/1]">
+            <li className="relative snap-start min-w-[400px] w-full aspect-[1/1]">
               <Image
                 key={idx}
                 src={image.responsiveImage.src}
@@ -33,10 +33,10 @@ export default function Slideshow({ images }: { images: Array<DatoImage> }) {
                 fill
                 className={`transition-transform duration-1000`}
               />
-            </div>
+            </li>
           )
         })}
-      </div>
+      </ul>
       <div className="flex justify-center gap-2 mt-2">
         {images.map((_, idx) => {
           return (
